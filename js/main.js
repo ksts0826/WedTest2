eef6b8ec7d880d6ad63309f8dfa965d8ed64a494
ksts0826 <ksts0826lv@gmail.com>
2023-05-09 19:04:33 +0900

    footerを修正

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ function include_header(){
         document.write(header_html); // herder.htmlを表示する
     });
 }
+function include_footer(){
+    $.ajax({
+        url: 'footer.html', // リクエストを送信するURLを指定
+        async: false, // 非同期リクエストを無効にする
+    }).done(function(footer_html){ // 通信が成功したら
+        document.write(footer_html); // footer.htmlを表示する
+    });
+}
